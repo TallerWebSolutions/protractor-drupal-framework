8d6021cb5744477642551b3e7d98ae9718a5437a
wlsf82 <wlsf82@gmail.com>
2015-07-16 21:50:40 -0300

    Created first page object called sample.page. This page has a get and a checkDrupalTheme methods so far.

---
+++ pages/sample.page.js
@@ -0,0 +1,24 @@
+/**
+* @file sample.page.js
+*/
+
+// This is a sample page file where you will find generic methods to use with drupal.
+var SamplePage = function () {
+
+  // The url argument is optional, if not set it just goes to the baseUrl defined in the conf.js file.
+	this.get = function (url) {
+    if (typeof(url) != 'undefined') {
+      browser.get('/' + url);
+    } else {
+      browser.get('/');
+    }
+  };
+
+  // Check that the correct drupal theme is used based on a string argument that represents the drupal theme.
+  this.checkDrupalTheme = function (theme) {
+    expect(browser.executeScript('return Drupal.settings.ajaxPageState.theme')).toEqual(theme);
+  };
+
+};
+
+module.exports = new SamplePage();
