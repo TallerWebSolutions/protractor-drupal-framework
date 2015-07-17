d0c9dfcb59a643a54a47455adf570beea538feb8
wlsf82 <wlsf82@gmail.com>
2015-07-16 22:57:27 -0300

    Added authentication page object for testing use.

---
+++ pages/authentication.page.js
@@ -0,0 +1,30 @@
+/**
+* @file authentication.page.js
+*/
+
+// This is a sample page file where you will find generic methods to use with drupal.
+var AuthenticationPage = function () {
+
+  this.usernameField = element(by.css('input#edit-name'));
+  this.passwordField = element(by.css('input#edit-pass'));
+  this.loginButton = element(by.css('input#edit-submit'));
+  this.loggedIn = element(by.css('body.logged-in'));
+
+	this.get = function () {
+    browser.get('user');
+  };
+
+  this.login = function (user, password) {
+    this.get();
+    this.usernameField.sendKeys(user);
+    this.passwordField.sendKeys(password);
+    this.loginButton.click();
+  }
+
+  this.logout = function () {
+    browser.get('user/logout');
+  }
+
+};
+
+module.exports = new AuthenticationPage();
