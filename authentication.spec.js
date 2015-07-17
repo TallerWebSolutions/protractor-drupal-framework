90719986c61ca2fc0925e862b8ea0f14f5cc81fd
wlsf82 <wlsf82@gmail.com>
2015-07-16 22:56:34 -0300

    Added tests for login and logout.

---
+++ authentication.spec.js
@@ -0,0 +1,24 @@
+/**
+* @file authentication.spec.js
+*/
+
+var AuthenticationPage = require('./pages/authentication.page');
+
+browser.ignoreSynchronization = true;
+
+// For each spec file is recommended to have just one describe.
+// A describe may the the description of a functionality/feature or even a web page, like home page, contact page, etc. It depends on the team work agreement
+describe ('Authentication' , function () {
+  it ('login', function () {
+    // To login two arguments are needed, user and password.
+		AuthenticationPage.login('user-here', 'password-here');
+    // Check that user is logged in.
+    expect(AuthenticationPage.loggedIn.isPresent()).toBe(true);
+	});
+
+  it ('logout', function () {
+    AuthenticationPage.logout();
+    // Check that user is logged out.
+    expect(AuthenticationPage.loggedIn.isPresent()).toBe(false);
+  });
+});
