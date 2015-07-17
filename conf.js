c93dfbb4aa507a1233420f93ef6bd904ce5a4af0
wlsf82 <wlsf82@gmail.com>
2015-07-16 21:49:09 -0300

    Created configuration file for protractor drupal framework.

---
+++ conf.js
@@ -0,0 +1,20 @@
+/**
+* @file conf.js
+*/
+
+exports.config = {
+  // The selenium address where the selenium server will be running.
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+
+  // The test files are stored into the specs array, separated by comma.
+  specs: ['spec.js'],
+
+  // Browser configuration.
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  // The url that will be used for the tests. With this you can call just the relative urls into the tests.
+  // This is also good for running tests in different environments. To do this you just have to change the url here.
+  baseUrl: 'http://drupal.org',
+};
