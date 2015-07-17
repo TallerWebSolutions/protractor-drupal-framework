171336868c9214c960831653aef73993f2e41700
wlsf82 <wlsf82@gmail.com>
2015-07-17 00:52:17 -0300

    Updated protractor config file to use jasmine spec reporter (on prepare) for a better test report.

--- conf.js
+++ conf.js
@@ -20,4 +20,16 @@ exports.config = {
   // The url that will be used for the tests. With this you can call just the relative urls into the tests.
   // This is also good for running tests in different environments. To do this you just have to change the url here.
   baseUrl: 'http://drupal.org/',
+
+  // Here you will set things that have to happen before start testing.
+  onPrepare: function () {
+    var SpecReporter = require('jasmine-spec-reporter');
+    // add jasmine spec reporter
+    jasmine.getEnv().addReporter(new SpecReporter({
+      displayFailuresSummary: true,
+      displayFailedSpec: true,
+      displaySuiteNumber: true,
+      displaySpecDuration: true
+    }));
+  }
 };
