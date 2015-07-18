69b66934051a99987a332176741316995f394abc
wlsf82 <wlsf82@gmail.com>
2015-07-18 00:23:11 -0300

    Added performance page object for cache cleaning test.

---
+++ pages/performance.page.js
@@ -0,0 +1,23 @@
+/**
+ * @performance.page.js
+ */
+
+// This is an object used for cache cleaning.
+var PerformancePage = function () {
+
+  // Define performance attributes.
+	this.clearAllCachesButton = element(by.css('#system-performance-settings #edit-clear'));
+
+  // Define performance methods.
+  this.get = function () {
+  	browser.get('admin/config/development/performance');
+  };
+
+  this.clearAllCaches = function () {
+  	this.clearAllCachesButton.click();
+  	browser.driver.sleep(3000);
+  }
+
+};
+
+module.exports = new PerformancePage();
