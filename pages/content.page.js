c2fe58541ac230c8c377e08083bf19fc3770394a
wlsf82 <wlsf82@gmail.com>
2015-07-19 18:14:08 -0300

    Added content page object for content tests.

---
+++ pages/content.page.js
@@ -0,0 +1,39 @@
+/**
+* @file content.page.js
+*/
+
+// This is an object used for mananing drupal contents.
+var ContentPage = function () {
+
+	// Define content attributes.
+	this.titleField = element(by.css('#views-exposed-form-admin-views-node-system-1 #edit-title'));
+  this.applyButton = element(by.css('#views-exposed-form-admin-views-node-system-1 #edit-submit-admin-views-node'));
+
+	//Define content methods.
+	this.get = function () {
+    browser.get('/admin/content');
+  };
+
+  this.filter = function (contentTitle) {
+    this.titleField.sendKeys(contentTitle);
+    browser.driver.sleep(100);
+    this.applyButton.click();
+    browser.driver.sleep(100);
+  };
+
+  this.remove = function (title) {
+    browser.driver.sleep(3000);
+    this.filter(title);
+    browser.driver.sleep(3000);
+    element(by.cssContainingText('a', title)).click().then(function () {
+      element(by.cssContainingText('.tabs a', 'Edit')).click().then(function () {
+        element(by.css('#edit-delete')).click().then(function () {
+          element(by.css('#edit-submit')).click();
+        });
+      });
+    });
+  };
+
+};
+
+module.exports = new ContentPage();
